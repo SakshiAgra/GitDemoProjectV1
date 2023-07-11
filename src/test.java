7def862fea40bbff6f26cf5984216339678fae06
Sakshi Agrawal <sakshia@adobe.com>
2023-07-11 12:10:17 +0530

    Initial commit

---
+++ src/test.java
@@ -0,0 +1,14 @@
+public class test {
+    public static void main(String[] args)
+    {
+        String hungry = "yes";
+        if(hungry == "yes")
+        {
+            System.out.println("I am hungry");
+        }
+        else
+        {
+            System.out.println("I am not hungry");
+        }
+    }
+}
